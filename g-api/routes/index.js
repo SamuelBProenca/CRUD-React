4edd09f8835f0973bf807dc1a2264c4ab944b4a4
Samuel <samuelnrito782@gmail.com>
2023-09-07 15:30:30 -0300

    inicio da construção da api

--- g-api/routes/index.js
+++ g-api/routes/index.js
@@ -1,9 +1,37 @@
+const { MongoClient } = require('mongodb');
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+// Rota de listagem geral 
+
+router.get('/', async (req,res) => {
+  try{
+    await client.connect();
+    const db = client.db('fatecdb');
+    const collection = db.collection('project-g');
+    const result = await collection.find({}).toArray();
+    res.status(200).json(result);//{}
+  }catch(erro){
+    console.log(erro);
+    res.status(500).json({ message : 'Ocorreu erro na conexão. Tente novamente mais tarde!'});
+  }finally{
+    await client.close();
+  };
+});
+
+// Rota para adicionar doc
+
+router.post('/add', async(req,res) => {
+    const doc = {
+        name : req.body.name,
+        age : req.body.age,
+        history : req.body.history,
+        illustration : req.body.illustration // Trocar posteriormente para metodo de upload de img
+    }
 });
 
 module.exports = router;
