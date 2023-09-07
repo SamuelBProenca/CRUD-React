9f6143227a70537c38429e8a69854b3bee817010
Samuel <samuelnrito782@gmail.com>
2023-09-07 17:33:32 -0300

    alterações no js

--- g-api/routes/index.js
+++ g-api/routes/index.js
@@ -1,37 +1,71 @@
+const express = require('express');
+const multer = require('multer');
 const { MongoClient } = require('mongodb');
-const url = "mongodb://localhost:27017";
+
+const url = "mongodb://127.0.0.1:27017";
 const client = new MongoClient(url);
+const router = express.Router();
 
-var express = require('express');
-var router = express.Router();
-
-
-// Rota de listagem geral 
-
-router.get('/', async (req,res) => {
-  try{
-    await client.connect();
-    const db = client.db('fatecdb');
-    const collection = db.collection('project-g');
-    const result = await collection.find({}).toArray();
-    res.status(200).json(result);//{}
-  }catch(erro){
-    console.log(erro);
-    res.status(500).json({ message : 'Ocorreu erro na conexão. Tente novamente mais tarde!'});
-  }finally{
-    await client.close();
-  };
+// Configurar o armazenamento para o multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Diretório onde os uploads serão armazenados
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop());
+  },
 });
 
-// Rota para adicionar doc
+const upload = multer({ storage: storage });
 
-router.post('/add', async(req,res) => {
-    const doc = {
-        name : req.body.name,
-        age : req.body.age,
-        history : req.body.history,
-        illustration : req.body.illustration // Trocar posteriormente para metodo de upload de img
+// Rota para adicionar um documento com uma ilustração
+router.post('/add', upload.single('illustration'), async (req, res) => {
+  const doc = {
+    name: req.body.name,
+    age: req.body.age,
+    history: req.body.history,
+    // illustration: req.file.filename, // O nome do arquivo no servidor
+  };
+
+  if (doc.name && doc.age && doc.history) {
+    try {
+      await client.connect();
+      const db = client.db('fatecdb');
+      const collection = db.collection('project-g');
+      const result = await collection.insertOne(doc);
+      if (result) {
+        // Retorne a URL da imagem, se necessário
+        const imageUrl = `/uploads/${req.file.filename}`;
+        res.status(200).json({ message: 'Doc inserido com sucesso!', imageUrl });
+      } else {
+        res.status(500).json({ message: 'Erro ao incluir o documento. Tente novamente' });
+      }
+    } catch (erro) {
+      console.log(erro);
+      res.status(500).json({ message: 'Ocorreu um erro ao incluir o DOC. Tente novamente mais tarde!' });
+    } finally {
+      await client.close();
     }
+  } else {
+    res.status(400).json({ message: 'Requisição inválida. Verifique os dados e tente novamente' });
+  }
+});
+
+
+router.get('/list', async(req,res) => {
+    try{
+        await client.connect();//promise
+        const db= client.db('fatecdb');
+        const collection= db.collection('project-g');
+        const result= await collection.find({}).toArray();
+        res.status(200).json(result);//{}
+      }catch(erro){
+        console.log(erro);
+        res.status(500).json({message: 'Ocorreu um erro ao atender a solicitação. Tente novamente mais tarde!'});
+      }finally{
+        await client.close();
+      }
 });
 
 module.exports = router;
